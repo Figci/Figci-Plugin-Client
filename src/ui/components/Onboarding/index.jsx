17c3fcab5217e909c11f0e44fb76b5f2d9da899c
seulgi chu <seulgichu14@gmail.com>
2024-02-16 01:28:57 +0900

    📦️ [Chore] 플러그인 초기 보일러플레이트 구조 작성
    플러그인 초기 보일러플레이트 구조 작성

---
+++ src/ui/components/Onboarding/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+function Onboarding() {
+  return <Wrapper>Onboarding</Wrapper>;
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+`;
+
+export default Onboarding;
