46318d0c8df408ca2c6a0c55586f5452cbab33d6
tjd985 <tjd985@gmail.com>
2024-02-18 04:09:20 +0900

    ✨ [Feat] clientStorage에 토큰 저장
    UI에서 postMessage로 전달한 accessToken을 샌드박스 clientStorage에 저장

--- src/plugin/code.js
+++ src/plugin/code.js
@@ -1,1 +1,7 @@
 figma.showUI(__html__, { width: 400, height: 540 });
+
+figma.ui.onmessage = async message => {
+  if (message.type === "SAVE_ACCESS_TOKEN") {
+    await figma.clientStorage.setAsync("accessToken", message.content);
+  }
+};
