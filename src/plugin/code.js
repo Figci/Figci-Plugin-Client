99e51132b501c0c02930da71506f4fe3ae6eda95
seulgi chu <seulgichu14@gmail.com>
2024-02-18 21:57:09 +0900

    ✨ [Feat] GET_ACCESS_TOKEN 로직 추가
    GET_ACCESS_TOKEN 로직 추가

--- src/plugin/code.js
+++ src/plugin/code.js
@@ -4,4 +4,13 @@ figma.ui.onmessage = async message => {
   if (message.type === "SAVE_ACCESS_TOKEN") {
     await figma.clientStorage.setAsync("accessToken", message.content);
   }
+
+  if (message.type === "GET_ACCESS_TOKEN") {
+    const accessToken = await figma.clientStorage.getAsync("accessToken");
+
+    figma.ui.postMessage({
+      type: "GET_ACCESS_TOKEN",
+      content: accessToken,
+    });
+  }
 };
