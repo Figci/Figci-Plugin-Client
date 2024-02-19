e33ca4c9a460ea05162c4bbbfe35904487fa0cb0
seulgi chu <seulgichu14@gmail.com>
2024-02-20 01:06:47 +0900

    ✨ [Feat] message type 설정 및 postMessage 추가
    message type 설정 및 postMessage 추가

--- src/plugin/code.js
+++ src/plugin/code.js
@@ -13,4 +13,33 @@ figma.ui.onmessage = async message => {
       content: accessToken,
     });
   }
+
+  if (message.type === "GET_PROJECT_KEY") {
+    const projectKey = figma.fileKey;
+
+    figma.ui.postMessage({
+      type: "GET_PROJECT_KEY",
+      content: projectKey,
+    });
+  }
+
+  if (message.type === "GET_CURRENT_PAGE_ID") {
+    const currentPageId = figma.currentPage.id;
+
+    figma.ui.postMessage({
+      type: "GET_CURRENT_PAGE_ID",
+      content: currentPageId,
+    });
+  }
+
+  if (message.type === "GET_NEW_VERSION_ID") {
+    const newVersionId = await figma.saveVersionHistoryAsync(
+      new Date().toISOString().split("T").join(" "),
+    );
+
+    figma.ui.postMessage({
+      type: "GET_NEW_VERSION_ID",
+      content: newVersionId,
+    });
+  }
 };
