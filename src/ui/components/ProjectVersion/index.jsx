01c4ccc2466f9c7a5ad4717f395128cc5b365da8
tjd985 <tjd985@gmail.com>
2024-02-20 04:42:24 +0900

    ✨ [Feat] version페이지 구현
    ProjectVersion컴포넌트에서 diffingResult받아오는 것 까지 구현

--- src/ui/components/ProjectVersion/index.jsx
+++ src/ui/components/ProjectVersion/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styled from "styled-components";
 
 import Button from "../shared/Button";
@@ -8,14 +8,132 @@ import ToastPopup from "../shared/Toast";
 import useProjectStore from "../../../store/project";
 import useProjectVersionStore from "../../../store/projectVersion";
 
+import postMessage from "../../../utils/postMessage";
+import createOption from "../../../utils/createOption";
+import isCommonPage from "../../../utils/isCommonPage";
+import getCommonPages from "../../../services/getCommonPages";
+import getDiffingResultQuery from "../../../services/getDiffingResultQuery";
+
+const [GET_CURRENT_PAGE_ID, GET_PROJECT_KEY, GET_ACCESS_TOKEN] = [
+  "GET_CURRENT_PAGE_ID",
+  "GET_PROJECT_KEY",
+  "GET_ACCESS_TOKEN",
+];
+
 function ProjectVersion() {
   const [toast, setToast] = useState({});
+  const [projectInformation, setProjectInformation] = useState({});
+  const [selectedVersion, setSelectedVersion] = useState("");
+  const [commonPageId, setCommonPageId] = useState("");
 
   const { project, setProject, clearProject } = useProjectStore();
   const { byDates, allDates } = useProjectVersionStore();
 
-  const handleChange = () => {};
-  const handleClick = () => {};
+  const {
+    data: diffingResult,
+    isLoading,
+    isError,
+    error,
+  } = getDiffingResultQuery(
+    projectInformation.projectKey,
+    projectInformation.beforeVersion,
+    projectInformation.afterVersion,
+    commonPageId,
+  );
+
+  const handleProjectInformation = ev => {
+    switch (ev.data.pluginMessage.type) {
+      case GET_CURRENT_PAGE_ID:
+        const pageId = ev.data.pluginMessage.content;
+
+        setProjectInformation(currentState => ({ ...currentState, pageId }));
+
+        break;
+
+      case GET_PROJECT_KEY:
+        const projectKey = ev.data.pluginMessage.content;
+
+        setProjectInformation(currentState => ({
+          ...currentState,
+          projectKey,
+        }));
+
+        break;
+
+      case GET_ACCESS_TOKEN:
+        const accessToken = ev.data.pluginMessage.content;
+
+        setProjectInformation(currentState => ({
+          ...currentState,
+          accessToken,
+        }));
+
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  const handleChange = ev => {
+    ev.preventDefault();
+
+    setSelectedVersion({
+      ...selectedVersion,
+      [ev.currentTarget.className]: ev.target.value,
+    });
+  };
+
+  const handleClick = async ev => {
+    ev.preventDefault();
+
+    if (!selectedVersion.beforeVersion) {
+      setToast({ status: true, messaeg: "선택하지 않은 버전이 존재합니다." });
+
+      return;
+    }
+
+    const { beforeVersionId } = selectedVersion;
+    const { afterVersionId } = project;
+    const { projectKey, accessToken } = projectInformation;
+
+    const responseResult = await getCommonPages(
+      projectKey,
+      beforeVersionId,
+      afterVersionId,
+      accessToken,
+    );
+
+    if (responseResult.result === "error") {
+      setToast({ status: true, messaeg: responseResult.message });
+
+      return;
+    }
+
+    const commonPageList = responseResult.content;
+    const currentPageId = projectInformation.pageId;
+
+    if (!isCommonPage(commonPageList, currentPageId)) {
+      setToast({
+        status: true,
+        message: "선택하신 버전에는 현재 페이지가 존재하지 않습니다!",
+      });
+    }
+  };
+
+  useEffect(() => {
+    clearProject();
+
+    postMessage(GET_CURRENT_PAGE_ID);
+    postMessage(GET_PROJECT_KEY);
+    postMessage(GET_ACCESS_TOKEN);
+
+    window.addEventListener("message", handleProjectInformation);
+
+    return () => {
+      window.removeEventListener("message", handleProjectInformation);
+    };
+  }, []);
 
   return (
     <>
@@ -46,6 +164,8 @@ function ProjectVersion() {
               <option value="" disabled selected>
                 버전 선택
               </option>
+              {selectedVersion.beforeDate &&
+                createOption(byDates[selectedVersion.beforeDate])}
             </select>
             <Description
               className="description"
@@ -54,24 +174,14 @@ function ProjectVersion() {
               text="지정한 버전 명이 없으면 시간으로 보여요!"
             />
           </label>
-          <div className="buttons">
-            <Button
-              handleClick={handleClick}
-              usingCase="line"
-              size="small"
-              className="prev"
-            >
-              이전
-            </Button>
-            <Button
-              handleClick={handleClick}
-              usingCase="solid"
-              size="small"
-              className="next"
-            >
-              다음
-            </Button>
-          </div>
+          <Button
+            handleClick={handleClick}
+            usingCase="solid"
+            size="small"
+            className="next"
+          >
+            비교하기
+          </Button>
         </form>
       </ContentsWrapper>
       {toast.status && (
