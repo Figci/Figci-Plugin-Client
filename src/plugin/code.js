dc8196905820e2e1694e362543f5dafd94c44529
tjd985 <tjd985@gmail.com>
2024-02-20 20:29:55 +0900

    ✨ [Feat] sandbox 렌더링 로직 작성
    UI로부터 변경사항을 수신 하여 변경사항을 시각화해서 렌더해주는 로직 작성

--- src/plugin/code.js
+++ src/plugin/code.js
@@ -1,5 +1,94 @@
 figma.showUI(__html__, { width: 400, height: 540 });
 
+const differenceRectangleIdList = [];
+
+figma.on("close", () => {
+  differenceRectangleIdList.forEach(rectangleId => {
+    const rectangleNode = figma.getNodeById(rectangleId);
+
+    rectangleNode.remove();
+  });
+
+  differenceRectangleIdList.length = 0;
+});
+
+const isOwnProperty = (targetObject, targetProperty) => {
+  return Object.prototype.hasOwnProperty.call(targetObject, targetProperty);
+};
+
+const renderDifferenceRectangle = (differences, modifiedFrames) => {
+  for (const nodeId in differences) {
+    if (isOwnProperty(differences, nodeId)) {
+      const modifiedNode = differences[nodeId];
+
+      const { type, differenceInformation, frameId } = modifiedNode;
+      const { x, y, width, height } = modifiedNode.position;
+
+      const differenceRectangle = figma.createRectangle();
+
+      differenceRectangle.resize(width, height);
+      differenceRectangle.x = x;
+      differenceRectangle.y = y;
+
+      switch (type) {
+        case "NEW":
+          differenceRectangle.fills = [
+            { type: "SOLID", color: { r: 0.435, g: 0.831, b: 0.505 } },
+          ];
+          differenceRectangle.opacity = 0.2;
+          differenceRectangle.setPluginData(
+            "differenceInformation",
+            "선택하신 이전 버전에는 존재하지 않는 노드 입니다!",
+          );
+
+          differenceRectangleIdList.push(differenceRectangle.id);
+          modifiedFrames[frameId].isNew = false;
+
+          break;
+        case "MODIFIED":
+          differenceRectangle.fills = [
+            { type: "SOLID", color: { r: 0.976, g: 0.407, b: 0.329 } },
+          ];
+          differenceRectangle.opacity = 0.2;
+          differenceRectangle.setPluginData(
+            "differenceInformation",
+            JSON.stringify(differenceInformation),
+          );
+
+          differenceRectangleIdList.push(differenceRectangle.id);
+          modifiedFrames[frameId].isNew = false;
+
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+  for (const frameId in modifiedFrames) {
+    if (isOwnProperty(modifiedFrames, frameId)) {
+      if (modifiedFrames[frameId].isNew) {
+        const differenceRectangle = figma.createRectangle();
+        const { x, y, width, height } = modifiedFrames[frameId];
+
+        differenceRectangle.resize(width, height);
+        differenceRectangle.x = x;
+        differenceRectangle.y = y;
+        differenceRectangle.fills = [
+          { type: "SOLID", color: { r: 0.435, g: 0.831, b: 0.505 } },
+        ];
+        differenceRectangle.opacity = 0.2;
+        differenceRectangle.setPluginData(
+          "differenceInformation",
+          "선택하신 이전 버전에는 존재하지 않는 프레임 입니다!",
+        );
+
+        differenceRectangleIdList.push(differenceRectangle.id);
+      }
+    }
+  }
+};
+
 figma.ui.onmessage = async message => {
   if (message.type === "SAVE_ACCESS_TOKEN") {
     await figma.clientStorage.setAsync("accessToken", message.content);
@@ -44,6 +133,10 @@ figma.ui.onmessage = async message => {
   }
 
   if (message.type === "POST_DIFFING_RESULT") {
-    const differences = message.content;
+    const { differences, modifiedFrames } = message.content;
+
+    differenceRectangleIdList.length = 0;
+
+    renderDifferenceRectangle(differences, modifiedFrames);
   }
 };
