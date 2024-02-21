04e01c1bb8088457a8c706d55c948886726ef4f9
seulgi chu <seulgichu14@gmail.com>
2024-02-22 00:18:40 +0900

    ✨ [Feat] Popup 컴포넌트 추가
    Popup 컴포넌트 레이아웃 및 기능 추가

---
+++ src/ui/components/shared/Popup.jsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+import Modal from "./Modal";
+import Description from "./Description";
+import Button from "./Button";
+
+function Popup({ title, text, path, buttonName, isOpenedPopup }) {
+  const navigate = useNavigate();
+
+  return (
+    <Modal>
+      <TextWrapper>
+        <h1 className="title">{title}</h1>
+        <Description size="medium" align="center" text={text} />
+      </TextWrapper>
+      <ButtonWrapper>
+        <Button
+          className="navigate-button"
+          size="small"
+          usingCase="solid"
+          handleClick={ev => {
+            ev.preventDefault();
+
+            navigate(path);
+          }}
+        >
+          {buttonName}
+        </Button>
+        <Button
+          className="close-button"
+          size="small"
+          usingCase="line"
+          handleClick={ev => {
+            ev.preventDefault();
+
+            isOpenedPopup(false);
+          }}
+        >
+          아니오
+        </Button>
+      </ButtonWrapper>
+    </Modal>
+  );
+}
+
+const ButtonWrapper = styled.div`
+  box-sizing: border-box;
+  display: grid;
+  grid-template-rows: repeat(2, 1fr);
+  row-gap: 8px;
+  padding: 0px 24px 12px;
+`;
+
+const TextWrapper = styled.div`
+  box-sizing: border-box;
+  width: 260px;
+  margin: 24px;
+
+  .title {
+    margin-bottom: 8px;
+
+    color: #000000;
+    font-size: 1rem;
+    font-weight: 900;
+    font-style: normal;
+    line-height: 24px;
+    text-align: center;
+  }
+`;
+
+export default Popup;
