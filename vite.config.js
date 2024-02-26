58b4b87ae25fded97ed79cb74e7dd8cc8791d715
kyeong-ko <rhrudwnszoq@naver.com>
2024-02-27 03:31:05 +0900

    📦️ [Chore] 플러그인 테스트 코드 보일러 플레이트

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,16 @@ import react from "@vitejs/plugin-react";
 import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
+  test: {
+    globals: true,
+    collectCoverageFrom: ["src/ui/**/*.jsx", "src/**/*.js"],
+    setupFiles: "src/spec/setupTests.js",
+    testMatch: ["./src/spec/**/*.spec.jsx"],
+    environment: "jsdom",
+    coverage: {
+      all: true,
+      exclude: ["*.config.js", "*.cjs", "./src/ui/main.jsx", "*.json"],
+    },
+  },
   plugins: [react(), viteSingleFile()],
 });
