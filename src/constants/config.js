7a2aae675a7e1ff49393a7b4835a5ded7d72ddcc
seulgi chu <seulgichu14@gmail.com>
2024-03-03 19:44:41 +0900

    ✏️ [Style] 변경 요소 텍스트 영문화
    변경 요소 한글 텍스트 영문화 작업 진행

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   DELAY_TIME_POLLING: 2000,
-  NEW_NODE: "이전 버전엔 없던 새로운 요소에요!",
-  NEW_FRAME: "이전 버전엔 없던 새로운 프레임이에요!",
+  NEW_NODE: "New element, not in the previous version!",
+  NEW_FRAME: "New frame, absent in the previous version!",
   TIME_GAP_MS: 9 * 60 * 60 * 1000,
 };
 
