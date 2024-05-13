68fdd42cb39ec7d0b5b4784976494d268b80b8f0
tjd985 <tjd985@gmail.com>
2024-05-13 15:18:12 +0900

    🚑️ [!HOTFIX] 피그마URL형식 변경에 따른 수정
    isValidFigmaUrl 정규표현식 수정

--- src/utils/isValidFigmaUrl.js
+++ src/utils/isValidFigmaUrl.js
@@ -1,6 +1,6 @@
 const isValidFigmaUrl = figmaUrl => {
   const figmaUrlPattern =
-    /^(?:https:\/\/)?(?:www\.)?figma\.com\/file\/([0-9a-zA-Z]{22,128})(?:\/?([^?]+)?(.*))?$/;
+    /^(?:https:\/\/)?(?:www\.)?figma\.com\/(?:file|design)\/([0-9a-zA-Z]{22,128})(?:\/([^?]+)?(\?.*)?)?$/;
 
   return figmaUrlPattern.test(figmaUrl);
 };
